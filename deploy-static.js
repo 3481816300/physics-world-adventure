79fcff2fe54debed9bf62ead2c162d3a4f25a2e4
Harry-Li <3481816300@qq.com>
2026-08-04 14:42:13 +0800

    上传 deploy-static.js

---
+++ deploy-static.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const path = require("path");
+
+const root = __dirname;
+const dist = path.join(root, "dist");
+const entries = ["index.html", "css", "js", "assets"];
+
+function copyDir(from, to) {
+  fs.mkdirSync(to, { recursive: true });
+  for (const entry of fs.readdirSync(from, { withFileTypes: true })) {
+    const src = path.join(from, entry.name);
+    const dest = path.join(to, entry.name);
+    if (entry.isDirectory()) {
+      copyDir(src, dest);
+    } else {
+      fs.copyFileSync(src, dest);
+    }
+  }
+}
+
+fs.rmSync(dist, { recursive: true, force: true });
+fs.mkdirSync(dist, { recursive: true });
+
+for (const entry of entries) {
+  const source = path.join(root, entry);
+  if (!fs.existsSync(source)) continue;
+  if (fs.statSync(source).isDirectory()) {
+    copyDir(source, path.join(dist, entry));
+  } else {
+    fs.copyFileSync(source, path.join(dist, entry));
+  }
+}
+
+console.log("Static build written to dist");
